0fcd978f5d01a906f891e79aa5e0af562a39d7dd
Woodson Delhia <woodsondelhia88@gmail.com>
2018-08-23 17:16:37 -0400

    Added Approval Process types

---
+++ src/Salesforce/ApprovalProcess/Types.purs
@@ -0,0 +1,54 @@
+module Salesforce.ApprovalProcess.Types where 
+
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
+import Foreign.Class (class Decode)
+import Foreign.Generic (defaultOptions, genericDecode)
+import Foreign.Generic.EnumEncoding (defaultGenericEnumOptions, genericDecodeEnum)
+import Prelude (class Show, ($))
+
+data ActionType = Submit | Approve | Reject 
+data ApprovalStatus = Approved | Rejected | Removed | Pending 
+
+newtype ApprovalRequest 
+    = ApprovalRequest { actionType                :: ActionType 
+                      , contextActorId            :: String 
+                      , contextId                 :: String 
+                      , comments                  :: String 
+                      , nextApproverIds           :: Array String 
+                      , processDefinitionNameOrId :: String 
+                      , skipEntryCriteria         :: Boolean 
+                      } 
+
+newtype ApprovalResponse 
+    = ApprovalResponse { actorIds       :: Array String 
+                       , entityId       :: String 
+                       , errors         :: Array String 
+                       , instanceId     :: String 
+                       , instanceStatus :: ApprovalStatus 
+                       , newWorkItemIds :: Array String 
+                       , success        :: Boolean
+                       } 
+
+derive instance genericActionType :: Generic ActionType _
+derive instance genericApprovalStatus :: Generic ApprovalStatus _ 
+derive instance genericApprovalRequest :: Generic ApprovalRequest _
+derive instance genericApprovalResponse :: Generic ApprovalResponse _ 
+
+instance decodeActionType :: Decode ActionType where
+  decode = genericDecodeEnum $ defaultGenericEnumOptions
+
+instance decodeApprovalStatus :: Decode ApprovalStatus where
+  decode = genericDecodeEnum $ defaultGenericEnumOptions
+
+instance decodeApprovalRequest :: Decode ApprovalRequest where
+  decode = genericDecode $ defaultOptions { unwrapSingleConstructors = true }
+
+instance showActionType :: Show ActionType where 
+    show = genericShow
+
+instance showApprovalStatus :: Show ApprovalStatus where 
+    show = genericShow
+
+instance showApprovalRequest :: Show ApprovalRequest where 
+    show = genericShow
